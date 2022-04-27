f69341154e214b8bb0cd4f63518e10d33bc0b0dd
Whaeedil <baoppyhossen1234@gmail.com>
2022-04-27 15:04:54 +0600

    redmi-file-update

--- src/__auth_steps.js
+++ src/__auth_steps.js
@@ -22,9 +22,9 @@
 
 /**
  * Firebase hosting steps
- * 1.npm install -g firebase-tools
- * 2.firebase login
- * 3.firebase init
- * 4.firebase deploy
- * 
+ * 1.npm install -g firebase-tools  (one time for you pc)
+ * 2.firebase login (one time for your pc)
+ * 3.firebase init ( one time for each project)
+ * 7.npm run build (every time you want to deploy)
+ * 8.firebase deploy (every time you want to deploy)
 */
